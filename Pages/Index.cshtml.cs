0c3329d8f4b449d4e1122f7c613f1451090bcecc
unknown <alaghbri.mohammed>
2023-09-25 03:03:42 +0300

    print headers

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -14,6 +14,19 @@ public class IndexModel : PageModel
 
     public void OnGet()
     {
+        var sb = new System.Text.StringBuilder();
+
+    // Loop through the Request.Headers dictionary
+    foreach (var header in Request.Headers)
+    {
+        // Append each header and its value to the StringBuilder
+        sb.AppendLine($"{header.Key} = {header.Value}");
+    }
+
+    // Assign the StringBuilder content to a property that you can use in your view
+    Headers = sb.ToString();
 
     }
+
+    public string Headers { get; set; }
 }
