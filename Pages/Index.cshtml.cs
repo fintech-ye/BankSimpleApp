684f2a9a36e4c5b7ff4e123d7c2fd5c1755f999c
unknown <alaghbri.mohammed>
2023-09-25 04:04:20 +0300

    add extra jwt info

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.IdentityModel.Tokens.Jwt;
 
 namespace asp_simple.Pages;
 
@@ -26,6 +27,12 @@ public class IndexModel : PageModel
             if(header.Key == "X-Auth-Request-User"){
                 UserName =  header.Value.ToString();
             }
+            if(header.Key == "X-Auth-Request-Access-Token"){
+                SetIdTokenExpiry(header.Value.ToString());
+                sb.AppendLine($"FullName = {FullName}");
+                sb.AppendLine($"SubscriberID = {SubID}");
+                sb.AppendLine($"Session Expiry = {SessionEnd}");
+            }
         }
 
     }
@@ -35,7 +42,18 @@ public class IndexModel : PageModel
 
     }
 
+    public void SetIdTokenExpiry(string idtoken)
+        {
+        var token = new JwtSecurityToken(jwtEncodedString: idtoken);
+        SessionEnd = token.Claims.First(c => c.Type == "expiry").Value;
+        FullName = token.Claims.First(c => c.Type == "name").Value;
+        SubID = token.Claims.First(c => c.Type == "sub").Value;
+    }
+    
     public string Headers { get; set; }
     public string UserName { get; set; }
+    public string FullName { get; set; }
+    public string SessionEnd { get; set; }
+    public string SubID { get; set; }
 }
 
